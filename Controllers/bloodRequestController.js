1da38c138e3fabb679bca37ce058655b87a62210
Doğa Kayra Yılmazarslan <94742646+Noxtrah@users.noreply.github.com>
2024-01-12 22:37:59 +0300

    API call is made via API Gateway

--- Controllers/bloodRequestController.js
+++ Controllers/bloodRequestController.js
@@ -37,6 +37,9 @@ export async function requestBlood(req, res) {
     console.log("Result: " + result);
 
     if (result.rowsAffected && result.rowsAffected[0] > 0) {
+
+      await invokeSearchBloodAPI();
+
       return res.status(201).json({ message: 'Blood request added successfully.' });
     } else {
       return res.status(500).json({ error: 'Failed to add blood request.' });
@@ -46,6 +49,30 @@ export async function requestBlood(req, res) {
     return res.status(500).json({ error: 'Internal Server Error' });
   }
 };
+
+async function invokeSearchBloodAPI() {
+  const apiGatewayBaseUrl = 'https://blooddonorsystemapigateway.azure-api.net';
+  const searchBloodEndpoint = '/api/search-blood';
+  const fullUrl = apiGatewayBaseUrl + searchBloodEndpoint;
+  try {
+    const searchBloodResponse = await fetch(fullUrl, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (searchBloodResponse.ok) {
+      const searchBloodData = await searchBloodResponse.json();
+      console.log('Search Blood API response:', searchBloodData);
+    } else {
+      console.error('Failed to invoke Search Blood API:', searchBloodResponse.statusText);
+    }
+  } catch (error) {
+    console.error('Error invoking Search Blood API:', error);
+  }
+}
+
 export async function getBloodRequests(req, res) {
   try {
     // Connect to the database
